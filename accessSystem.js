ab953881bf741110c2567ffa2026c8e6af75e546
AwallenceCode <awallencecode@gmail.com>
2026-01-20 15:29:42 +0200

    the framework for future program expansion has been written

---
+++ accessSystem.js
@@ -0,0 +1,84 @@
+const prompt = require("prompt-sync")();
+
+//method for verifying the user's age
+function checkAge()
+{
+
+}
+
+//method for verifying the ability to purchase alcohol
+function buyAlcohol()
+{
+
+}
+
+//method for checking driving ability
+function driveCar()
+{
+
+}
+
+//method for verifying a subscription
+function checkSubscription()
+{
+
+}
+
+//method for checking video playback capability
+function watchMovie()
+{
+
+}
+
+//method for checking the ability to write comments
+function writeComments()
+{
+
+}
+
+//function for initiating actions and checks
+function runAction()
+{
+
+}
+
+//the main method that delegates the entire program
+function main()
+{
+    let userChoise;
+
+    while(true)
+    {
+        switch (userChoise)
+        {
+            case 1:
+                {
+                    break;
+                }
+            
+            case 2:
+                {
+                    break;
+                }
+
+            case 3:
+                {
+                    break;
+                }
+
+            case 4:
+                {
+                    console.log("\nYou have exited the program. Have a nice day!");           
+                    return;
+                }
+
+            default:
+                {
+                    console.log("There is no such answer option. Please try again...");
+                    break;
+                }
+        }
+    }
+}
+
+main();
