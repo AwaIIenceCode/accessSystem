54a4d49910a9fc7f9e8b1582b768a26cef3e1a04
AwallenceCode <awallencecode@gmail.com>
2026-01-20 15:59:24 +0200

    Refactor checkAge function to simplify age verification logic

--- accessSystem.js
+++ accessSystem.js
@@ -17,8 +17,9 @@ function enterUserAge()
 
 //method for verifying the user's age
 function checkAge()
-{
-
+{    
+    if (userAge < 18) { return false; }
+    if (userAge >= 18) { return true; } 
 }
 
 //method for verifying the ability to purchase alcohol
